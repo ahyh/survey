4539ff4d5a39314273a13758ffa08a5afa826cd2
ahyanhuan <ahyanhuan@sina.com>
2018-10-01 21:33:45 +0800

    quartz定时任务创建表

---
+++ survey-web/src/main/java/com/yh/survey/log/quartz/LogCreateJob.java
@@ -0,0 +1,31 @@
+package com.yh.survey.log.quartz;
+
+import com.yh.survey.log.interf.LogService;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class LogCreateJob extends QuartzJobBean {
+
+    private LogService logService;
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        RoutingKeyBinder.bindKey(RoutingKeyBinder.DATASOURCE_LOG);
+        logService.createTable(1);
+
+//        RoutingKeyBinder.bindKey(RoutingKeyBinder.DATASOURCE_LOG);
+        logService.createTable(2);
+
+//        RoutingKeyBinder.bindKey(RoutingKeyBinder.DATASOURCE_LOG);
+        logService.createTable(3);
+    }
+
+    public LogService getLogService() {
+        return logService;
+    }
+
+    public void setLogService(LogService logService) {
+        this.logService = logService;
+    }
+}
